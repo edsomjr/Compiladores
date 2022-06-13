121be7f79c3b08d21502f2239b9e048ccb60bfd5
Edson Alves <edsomjr@gmail.com>
2022-06-13 19:41:00 -0300

    Adição do texto sobre gramáticas.

---
+++ analise_sintatica/codes/gcc.sh
@@ -0,0 +1,6 @@
+codes/erro.cpp: In function ‘int main()’:
+codes/erro.cpp:7:26: error: expected ‘;’ before ‘:’ token
+    7 |     cout << "Hello World":
+      |                          ^
+      |                          ;
+compilation terminated due to -Wfatal-errors.
